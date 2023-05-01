c323f83dc367564655b749cfaf27a600a45b4ab7
HEEBOK\gmlqh <gmlqhr95@gmail.com>
2023-05-01 16:17:48 +0900

    first commit

---
+++ src/main/java/com/Allryojo/arj/ProjectArjApplication.java
@@ -0,0 +1,13 @@
+package com.Allryojo.arj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectArjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectArjApplication.class, args);
+	}
+
+}
