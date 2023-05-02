4efe55c3680bfd34b2adb49d087302f008705ac2
wooorim <woorim0923@gmail.com>
2023-05-02 17:55:44 +0900

    travel main 페이지 작

---
+++ src/main/java/com/Allryojo/arj/controller/TravelController.java
@@ -0,0 +1,20 @@
+package com.Allryojo.arj.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/travel")
+
+public class TravelController {
+	@GetMapping("/main")
+	public String travel_main() {
+		log.info("travel_main()..");
+	
+		return "travel/travel_main";
+	}
+}
---
+++ src/test/java/com/Allryojo/arj/ProjectArjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Allryojo.arj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectArjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
